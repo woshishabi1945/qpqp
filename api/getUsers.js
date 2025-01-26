7360dbbc21d2437dbafd9d824cf35f20263623de
YFcJNT <144309953+woshishabi1945@users.noreply.github.com>
2025-01-26 16:24:24 +0800

    Update getUsers.js

--- api/getUsers.js
+++ api/getUsers.js
@@ -1,7 +1,8 @@
-let users = require('./logClick').users; // 引用用户记录
+import { getUsers } from './logClick'; // 从 logClick.js 引入 getUsers 函数
 
 export default function handler(req, res) {
   if (req.method === 'GET') {
+    const users = getUsers(); // 获取用户记录
     return res.status(200).json(users);
   }
   return res.status(405).json({ error: 'Method Not Allowed' });
